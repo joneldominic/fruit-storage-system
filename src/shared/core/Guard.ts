9740609075e9dde97412f5fb0c86971713b2d20e
Jonel Dominic Tapang <joneldominictapang.dev@gmail.com>
2023-06-29 02:44:35 +0800

    feat(guard.ts): added greaterThanOrEqual guard method

--- src/shared/core/Guard.ts
+++ src/shared/core/Guard.ts
@@ -26,6 +26,14 @@ export class Guard {
         );
   }
 
+  public static greaterThanOrEqual(minValue: number, actualValue: number): Result<GuardResponse> {
+    return actualValue >= minValue
+      ? Result.ok<GuardResponse>()
+      : Result.fail<GuardResponse>(
+          `Number given {${actualValue}} is not greater than or equal to {${minValue}}`
+        );
+  }
+
   public static againstAtLeast(numChars: number, text: string): Result<GuardResponse> {
     return text.length >= numChars
       ? Result.ok<GuardResponse>()
