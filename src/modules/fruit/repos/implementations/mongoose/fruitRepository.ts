3f1aaf110b3c7ee7999ec9c434d39170e807da17
Jonel Dominic Tapang <joneldominictapang.dev@gmail.com>
2023-07-01 01:56:24 +0800

    feat(fruitrepository): added FruitRepository mongoose implementation

---
+++ src/modules/fruit/repos/implementations/mongoose/fruitRepository.ts
@@ -0,0 +1,29 @@
+import { MongooseModels } from '../../../../../shared/infrastructure/database/mongoose/models';
+import Fruit from '../../../domain/fruit';
+import FruitMap from '../../../mappers/fruitMap';
+import IFruitRepository from '../../fruitRepository';
+
+export default class FruitRepository implements IFruitRepository {
+  private models: MongooseModels;
+
+  constructor(models: MongooseModels) {
+    this.models = models;
+  }
+
+  async exists(name: string): Promise<boolean> {
+    const FruitModel = this.models.Fruit;
+    const fruit = await FruitModel.findOne({ name }).lean();
+
+    return !!fruit;
+  }
+
+  async save(fruit: Fruit): Promise<void> {
+    const FruitModel = this.models.Fruit;
+
+    const exists = await this.exists(fruit.name.value);
+    if (exists) throw new Error('Fruit already exists.');
+
+    const rawMongooseFruit = FruitMap.toPersistence(fruit);
+    await FruitModel.create(rawMongooseFruit);
+  }
+}
