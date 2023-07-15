4526bf061482e297bbadc843d972b05380f9ec32
Jonel Dominic Tapang <joneldominictapang.dev@gmail.com>
2023-07-16 01:18:03 +0800

    test(outboxmessage): added outboxmessage entity test

---
+++ test/shared/domain/outbox/outboxMessage.test.ts
@@ -0,0 +1,44 @@
+import { describe, it, expect } from '@jest/globals';
+import UniqueEntityID from '../../../../src/shared/domain/UniqueEntityID';
+import OutboxMessage from '../../../../src/shared/domain/outbox/outboxMessage';
+
+describe('OutboxMessage ', () => {
+  it('should be able to create OutboxMessage successfully', async () => {
+    // Arrange
+    const outboxMessageUniqueID = new UniqueEntityID();
+    const outboxMessage = {
+      eventId: 'TEST EVENT',
+      status: 0,
+      message: 'TEST MESSAGE',
+      dateCreated: new Date()
+    };
+
+    // Act
+    const outboxMessageOrError = OutboxMessage.create(outboxMessage, outboxMessageUniqueID);
+
+    // Assert
+    expect(outboxMessageOrError.isSuccess).toBeTruthy();
+    expect(outboxMessageOrError.getValue().id).toBe(outboxMessageUniqueID);
+    expect(outboxMessageOrError.getValue().eventId).toBe(outboxMessage.eventId);
+    expect(outboxMessageOrError.getValue().status).toBe(outboxMessage.status);
+    expect(outboxMessageOrError.getValue().message).toBe(outboxMessage.message);
+    expect(outboxMessageOrError.getValue().dateCreated).toBe(outboxMessage.dateCreated);
+  });
+
+  it('should fail creating OutboxMessage when some props is null or undefined', async () => {
+    // Arrange
+    const outboxMessageUniqueID = new UniqueEntityID();
+    const outboxMessage = {
+      eventId: 'TEST EVENT',
+      status: 0,
+      message: null,
+      dateCreated: new Date()
+    };
+
+    // Act
+    const outboxMessageOrError = OutboxMessage.create(outboxMessage, outboxMessageUniqueID);
+
+    // Assert
+    expect(outboxMessageOrError.isFailure).toBeTruthy();
+  });
+});
