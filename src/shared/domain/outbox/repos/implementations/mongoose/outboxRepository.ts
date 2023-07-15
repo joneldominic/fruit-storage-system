c183e50cec15fc203d14c60cc1a44b32200360d0
Jonel Dominic Tapang <joneldominictapang.dev@gmail.com>
2023-07-16 01:15:34 +0800

    feat(mongoose outbox repo): added outbox repository - mongoose implementation

---
+++ src/shared/domain/outbox/repos/implementations/mongoose/outboxRepository.ts
@@ -0,0 +1,43 @@
+import { MongooseModels } from '../../../../../infrastructure/database/mongoose/models';
+import OutboxMessage from '../../../outboxMessage';
+import OutboxMessageStatus from '../../../outboxMessageStatus';
+import IOutboxRepository from '../../outboxRepository';
+import OutboxMessageMapper from '../../../mappers/outboxMessageMapper';
+
+export default class OutboxRepository implements IOutboxRepository {
+  private models: MongooseModels;
+
+  constructor(models: MongooseModels) {
+    this.models = models;
+  }
+
+  async addMessage(message: OutboxMessage): Promise<void> {
+    const OutboxMessageModel = this.models.OutboxMessage;
+    const outboxMessage = OutboxMessageMapper.toPersistence(message);
+
+    await OutboxMessageModel.create(outboxMessage);
+  }
+
+  async getPendingMessages(): Promise<OutboxMessage[]> {
+    const OutboxMessageModel = this.models.OutboxMessage;
+
+    const pendingMessages = await OutboxMessageModel.find({
+      status: OutboxMessageStatus.PENDING
+    }).lean();
+
+    const rawPendingMessages: OutboxMessage[] = pendingMessages.map((message) =>
+      OutboxMessageMapper.toDomain(message)
+    );
+
+    return rawPendingMessages;
+  }
+
+  async markMessageAsPublished(message: OutboxMessage): Promise<void> {
+    const OutboxMessageModel = this.models.OutboxMessage;
+
+    const filter = { id: message.id };
+    const update = { status: OutboxMessageStatus.DONE };
+
+    await OutboxMessageModel.updateOne(filter, update);
+  }
+}
