7f3e7f788924e6216d4f4b7cc5533ae16f8c8f9c
Jonel Dominic Tapang <joneldominictapang.dev@gmail.com>
2023-07-16 01:12:38 +0800

    feat(outboxmessagemapper): added outbox message mapper

---
+++ src/shared/domain/outbox/mappers/outboxMessageMapper.ts
@@ -0,0 +1,33 @@
+import UniqueEntityID from '../../UniqueEntityID';
+import Mapper from '../../../mapper/Mapper';
+import OutboxMessage from '../outboxMessage';
+
+export default class OutboxMessageMapper implements Mapper<OutboxMessage> {
+  public static toDomain(raw: any): OutboxMessage {
+    const outboxMessageOrError = OutboxMessage.create(
+      {
+        eventId: raw.eventId,
+        status: raw.status,
+        message: raw.message,
+        dateCreated: raw.dateCreated
+      },
+      new UniqueEntityID(raw.id)
+    );
+
+    if (outboxMessageOrError.isFailure || !outboxMessageOrError.isSuccess) {
+      console.error(outboxMessageOrError.getErrorValue());
+    }
+
+    return outboxMessageOrError.getValue();
+  }
+
+  public static toPersistence(outboxMessage: OutboxMessage): any {
+    return {
+      id: outboxMessage.id,
+      eventId: outboxMessage.eventId,
+      status: outboxMessage.status,
+      message: outboxMessage.message,
+      dateCreated: outboxMessage.dateCreated
+    };
+  }
+}
