c8e5e6fa9348922d638cd382edf9b7b1984aeee6
Jonel Dominic Tapang <joneldominictapang.dev@gmail.com>
2023-07-24 00:23:26 +0800

    feat(consumer setup): setup fruit storage consumer in app

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import app from './shared/infrastructure/http/app';
 
 // Jobs
-import './shared/infrastructure/outbox/implementation/node-cron/index';
+import './shared/infrastructure/outbox/implementation/node-cron';
+
+// Consumers
+import './modules/storage/consumers';
 
 app.start();
