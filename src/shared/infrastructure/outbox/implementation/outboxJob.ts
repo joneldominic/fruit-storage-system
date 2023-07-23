33acd36fbda8764a99eebf345707c0c6cf9e7aa2
Jonel Dominic Tapang <joneldominictapang.dev@gmail.com>
2023-07-24 00:20:56 +0800

    feat(producer-outbox integration): integrate fruit created event producer with outbox

--- src/shared/infrastructure/outbox/implementation/outboxJob.ts
+++ src/shared/infrastructure/outbox/implementation/outboxJob.ts
@@ -1,5 +1,7 @@
+import { FRUIT_CREATED_EVENT_ID } from '../../../../modules/fruit/domain/events/fruitCreated';
 import IOutboxRepository from '../../../domain/outbox/repos/outboxRepository';
 import IOutboxJob from '../outboxJob';
+import { fruitCreatedProducer } from './producersRegistry';
 
 export default class OutboxJob implements IOutboxJob {
   private outboxRepository: IOutboxRepository;
@@ -10,6 +12,17 @@ export default class OutboxJob implements IOutboxJob {
 
   async execute(): Promise<void> {
     const pendingMessages = await this.outboxRepository.getPendingMessages();
-    console.info('👨‍💻 Joneeeeel ~ file: outboxJob.ts:14 ~ pendingMessages:', pendingMessages);
+
+    pendingMessages.forEach(async (message) => {
+      switch (message.eventId) {
+        case FRUIT_CREATED_EVENT_ID:
+          fruitCreatedProducer.produce(message);
+          break;
+        default:
+          throw new Error('Invalid Event');
+      }
+
+      await this.outboxRepository.markMessageAsPublished(message);
+    });
   }
 }
---
+++ src/shared/infrastructure/outbox/implementation/producersRegistry.ts
@@ -0,0 +1,4 @@
+import FruitCreatedProducer from '../../../../modules/fruit/producers/fruitCreatedProducer';
+
+// eslint-disable-next-line import/prefer-default-export
+export const fruitCreatedProducer = new FruitCreatedProducer();
